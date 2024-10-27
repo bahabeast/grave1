029da5e7573eda2c66c2c46238b5fa54188adeb1
bahabeast <78960905+bahabeast@users.noreply.github.com>
2024-10-28 03:32:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const calculateButton = document.getElementById("calculateButton");
+    calculateButton.addEventListener("click", showPrice);
+});
+
 function showPrice() {
-    let edx = parseFloat(document.getElementById("education").value);
-    let moneyx = parseFloat(document.getElementById("wallet").value);
-    let statusx = parseFloat(document.getElementById("caste").value);
-    let bonus = statusx;
-    let repminus =1;
-    let agex = 1;
-    if (document.getElementById("music").checked){
-        bonus += 10;
-    } 
-    if (document.getElementById("cook").checked){
-        bonus += 20;
-    }
-    if (document.getElementById("character").checked){
-        bonus += 15;
-    } 
-    if (document.getElementById("sing").checked){
-        bonus += 10;
-    }
-    let ageElems = document.getElementsByName('age');
+    const getCheckboxValuesFilterReduce = (nodeList, initialValue) => {
+        let list = Array.from(nodeList).filter(item => item.checked);
+        return list.reduce((accumulator, item) => accumulator + Number(item.value), initialValue);
+    };
+
+    let edx = parseFloat(document.getElementById("education").value)
+    let moneyx = parseFloat(document.getElementById("wallet").value)
+    let statusx = parseFloat(document.getElementById("caste").value)
+    let repminus = 1;
+    let bonus = 0;
+
+    let name = document.getElementById("partnername").value;
+    let love_letter = document.getElementById("loveletter").value;
+    let price = parseFloat(document.getElementById("startingbid").value)
+
+    let person = {
+        bride_name: name,
+        bride_price: result,
+        letter_to_bride: love_letter,
+    };
+
+    let skills = document.querySelectorAll(".skills");
+    bonus = getCheckboxValuesFilterReduce(skills, bonus) + statusx;
+
+    const ageElems = document.getElementsByName('age');
+    ageElems.forEach(item => {
+        if (item.checked) {
+            repminus *= Number(item.value);
+        }
+    });
 
     if (document.getElementById("parent").checked) {
         repminus *= 0.85;
-    }  
-    if (document.getElementById("character").checked){
+    }
+
+    if (document.getElementById("characters").checked) {
         repminus *= 0.9;
-    } 
-    if (document.getElementById("general").checked){
+    }
+
+    if (document.getElementById("general").checked) {
         bonus -= 20;
     }
-    for (let i = 0; i < ageElems.length; i++) {
-        if (ageElems[i].checked) {
-            agex = parseFloat(ageElems[i].value);
-            break;
-        }
-    }
-    let cost = (edx * moneyx  * agex*repminus*1)*100 + (bonus);
-    document.getElementById("result").textContent = cost.toFixed(1);
+
+    let cost = (edx * moneyx * repminus) * (bonus + price)
+    document.getElementById("result").innerHTML = `The price for your bride ${person.bride_name} is ${cost.toFixed(1)}. Your love letter is "${person.letter_to_bride}"`
 }
